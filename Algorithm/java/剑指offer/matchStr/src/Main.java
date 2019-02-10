da565a12e5e095637e136f4d69806b64e457957f
leosanqing <stormleo@qq.com>
2019-02-10 22:28:40 +0800

    字符串匹配

---
+++ Algorithm/java/剑指offer/matchStr/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean match(char str[],char pattern[]){
+        if(str==null||pattern==null)
+            return false;
+        
+
+        return matchCore(str,pattern);
+    }
+
+    private static boolean matchCore(char[] str,char[] patten) {
+        return false;
+    }
+}
