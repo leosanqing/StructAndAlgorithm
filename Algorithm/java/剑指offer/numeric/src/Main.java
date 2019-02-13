d82b5abc2541edc00c611aa1d0bee6c44840f2ac
leosanqing <stormleo@qq.com>
2019-02-13 21:53:22 +0800

    2.13

--- Algorithm/java/剑指offer/numeric/src/Main.java
+++ Algorithm/java/剑指offer/numeric/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+
         StringBuffer sb=new StringBuffer();
         sb.append("1E3");
         char[]chars=sb.toString().toCharArray();
