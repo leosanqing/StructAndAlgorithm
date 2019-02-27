937c93c72381faad3126f15a1cb9eef498eea36c
leosanqing <stormleo@qq.com>
2019-02-27 16:30:23 +0800

    2.27

--- Algorithm/java/剑指offer/findGreatestSumInArray/src/Main.java
+++ Algorithm/java/剑指offer/findGreatestSumInArray/src/Main.java
@@ -10,7 +10,6 @@ public class Main {
         System.out.println(findGreatestSumInArray(nums));
 
 
-        System.out.println(CountOne2(11));
     }
 
 
@@ -35,29 +34,6 @@ public class Main {
         return greatestSum;
     }
 
-    public static long CountOne2(long n) {
-        long count = 0; // 1的个数
-        long i = 1;  // 当前位
-        long current = 0,after = 0,before = 0;
 
-        while((n / i) != 0) {
-            before = n / (i * 10); // 高位
-            current = (n / i) % 10; // 当前位
-            after = n - (n / i) * i;  // 低位
-
-            if (current == 0)
-                //如果为0,出现1的次数由高位决定,等于高位数字 * 当前位数
-                count = count + before * i;
-            else if(current == 1)
-                //如果为1,出现1的次数由高位和低位决定,高位*当前位+低位+1
-                count = count + before * i + after + 1;
-            else if (current > 1)
-                // 如果大于1,出现1的次数由高位决定,（高位数字+1）* 当前位数
-                count = count + (before + 1) * i;
-            //前移一位
-            i = i * 10;
-        }
-        return count;
-    }
 
 }
---
+++ Algorithm/java/剑指offer/numToString/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int numToString(int num){
+        if(num<0)
+            return 0;
+    }
+}
